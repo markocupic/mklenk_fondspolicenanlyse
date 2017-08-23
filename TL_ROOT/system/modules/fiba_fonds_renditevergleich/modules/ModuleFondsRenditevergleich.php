f1e9339b91b91c7f22f564a01205a763db57d6e4
Marko Cupic <m.cupic@gmx.ch>
2017-08-23 16:25:44 +0200

    bugfix after email from 23.08.2017

--- TL_ROOT/system/modules/fiba_fonds_renditevergleich/modules/ModuleFondsRenditevergleich.php
+++ TL_ROOT/system/modules/fiba_fonds_renditevergleich/modules/ModuleFondsRenditevergleich.php
@@ -11,231 +11,251 @@ namespace Markocupic;
 
 class ModuleFondsRenditevergleich extends \Module
 {
-    /**
-     * Template
-     * @var string
-     */
-    protected $strTemplate = 'mod_fonds_renditevergleich';
+	/**
+	 * Template
+	 * @var string
+	 */
+	protected $strTemplate = 'mod_fonds_renditevergleich';
 
-    /**
-     * Template
-     * @var string
-     */
-    protected $strTemplateAjax = 'mod_fonds_renditevergleich_ajax';
+	/**
+	 * Template
+	 * @var string
+	 */
+	protected $strTemplateAjax = 'mod_fonds_renditevergleich_ajax';
 
-    /**
-     * isAjaxRequest
-     * @var bool
-     */
-    protected $isAjax = false;
-
-
-    /**
-     * Display a wildcard in the back end
-     *
-     * @return string
-     */
-    public function generate()
-    {
-        if (TL_MODE == 'BE')
-        {
-            /** @var \BackendTemplate|object $objTemplate */
-            $objTemplate = new \BackendTemplate('be_wildcard');
-
-            $objTemplate->wildcard = '### ' . utf8_strtoupper($GLOBALS['TL_LANG']['FMD']['fonds_renditevergleich'][0]) . ' ###';
-            $objTemplate->title = $this->headline;
-            $objTemplate->id = $this->id;
-            $objTemplate->link = $this->name;
-            $objTemplate->href = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id=' . $this->id;
-
-            return $objTemplate->parse();
-        }
-
-        if (\Environment::get('isAjaxRequest') && $this->Input->get('FORM_SUBMIT') == 'fondsRenditevergleichFilterForm')
-        {
-            // Use an other template for Ajax calls
-            $this->isAjax = true;
-            $this->strTemplate = $this->strTemplateAjax;
-
-            // Bypass Search Index for Ajax calls to prevent duplicate content error
-            global $objPage;
-            $objPage->noSearch = 1;
-        }
-        // Load Language File for thead
-        \System::loadLanguageFile('tl_fonds_renditevergleich');
+	/**
+	 * isAjaxRequest
+	 * @var bool
+	 */
+	protected $isAjax = false;
 
 
-        return parent::generate();
-    }
+	/**
+	 * Display a wildcard in the back end
+	 *
+	 * @return string
+	 */
+	public function generate()
+	{
+		if (TL_MODE == 'BE')
+		{
+			/** @var \BackendTemplate|object $objTemplate */
+			$objTemplate = new \BackendTemplate('be_wildcard');
+
+			$objTemplate->wildcard = '### ' . utf8_strtoupper($GLOBALS['TL_LANG']['FMD']['fonds_renditevergleich'][0]) . ' ###';
+			$objTemplate->title = $this->headline;
+			$objTemplate->id = $this->id;
+			$objTemplate->link = $this->name;
+			$objTemplate->href = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id=' . $this->id;
+
+			return $objTemplate->parse();
+		}
+
+		if (\Environment::get('isAjaxRequest') && $this->Input->get('FORM_SUBMIT') == 'fondsRenditevergleichFilterForm')
+		{
+			// Use an other template for Ajax calls
+			$this->isAjax = true;
+			$this->strTemplate = $this->strTemplateAjax;
+
+			// Bypass Search Index for Ajax calls to prevent duplicate content error
+			global $objPage;
+			$objPage->noSearch = 1;
+		}
+		// Load Language File for thead
+		\System::loadLanguageFile('tl_fonds_renditevergleich');
 
 
-    /**
-     * Generate the module
-     */
-    protected function compile()
-    {
-        // Get all providers as an array (this is used in the filter form for the options in the provider select menu
-        $this->Template->arrAnbieter = $this->getAnbieter();
-
-        $fieldRendite = $this->Input->get('rendite') ?: 'rendite3Jahre';
+		return parent::generate();
+	}
 
 
-        // Handle the checkboxes in the filter form
-        $arrTranslate = array(
-            'etf_aktienfonds' => 'ETF Aktienfonds',
-            'etf_rentenfonds' => 'ETF Rentenfonds',
-            'dimensional_aktienfonds' => 'Dimensional Aktienfonds',
-            'dimensional_rentenfonds' => 'Dimensional Rentenfonds'
-        );
-        $arrWhere = array();
-        $strWhere = '';
-        foreach ($arrTranslate as $k => $v)
-        {
-            if (\Input::get($k) == 1)
-            {
-                $arrWhere[] = "fondsart='" . $arrTranslate[$k] . "'";
-            }
-        }
-        if (count($arrWhere) > 0)
-        {
-            $strWhere = ' WHERE (' . implode(' OR ', $arrWhere) . ')';
-        }
-        // This is a bit "buggy" but it works ;-)
-        // Do not show any datarecords if there are no checkboxes cheched
-        if ($this->isAjax && $strWhere == '')
-        {
-            $strWhere = " WHERE anbieter='DO NOT SHOW ANY DATARECORDS BECAUSE THERE ARE NO CHECKBOXES CHECKED'";
-        }
+	/**
+	 * Generate the module
+	 */
+	protected function compile()
+	{
+		// Get all providers as an array (this is used in the filter form for the options in the provider select menu
+		$this->Template->arrAnbieter = $this->getAnbieter();
+
+		$fieldRendite = $this->Input->get('rendite') ?: 'rendite3Jahre';
 
 
-        // Handle the "Anbieter" Select menu in the filter form
-        $strAnbieter = '';
-        if (\Input::get('anbieter') != '')
-        {
-            $strAnbieter = "anbieter LIKE '%" . urldecode(\Input::get('anbieter')) . "%'";
-            $strWhere = $strWhere == '' ? ' WHERE ' . $strAnbieter : $strWhere . ' AND ' . $strAnbieter;
-        }
+		// Handle the checkboxes in the filter form
+		$arrTranslate = array(
+			'etf_aktienfonds' => 'ETF Aktienfonds',
+			'etf_rentenfonds' => 'ETF Rentenfonds',
+			'dimensional_aktienfonds' => 'Dimensional Aktienfonds',
+			'dimensional_rentenfonds' => 'Dimensional Rentenfonds'
+		);
+		$arrWhere = array();
+		$strWhere = '';
+		foreach ($arrTranslate as $k => $v)
+		{
+			if (\Input::get($k) == 1)
+			{
+				$arrWhere[] = "fondsart='" . $arrTranslate[$k] . "'";
+			}
+		}
+		if (count($arrWhere) > 0)
+		{
+			$strWhere = ' WHERE (' . implode(' OR ', $arrWhere) . ')';
+		}
+		// This is a bit "buggy" but it works ;-)
+		// Do not show any datarecords if there are no checkboxes cheched
+		if ($this->isAjax && $strWhere == '')
+		{
+			$strWhere = " WHERE anbieter='DO NOT SHOW ANY DATARECORDS BECAUSE THERE ARE NO CHECKBOXES CHECKED'";
+		}
 
-        $chartX = [];
-        $chartY = [];
-        $chartBg = [];
-        $chartBorder = [];
 
-        $strQuery = "SELECT * FROM tl_fonds_renditevergleich" . $strWhere . ' ORDER BY ' . $fieldRendite . ' DESC';
-        $objDb = $this->Database->prepare($strQuery)->execute();
 
-        $i = 0;
-        $rows = array();
-        while ($objDb->next())
-        {
-            $rows[] = $objDb->row();
 
-            /**
-             * !important
-             * The configuration options for the horizontal bar chart are the same as for the bar chart.
-             * However, any options specified on the x axis in a bar chart, are applied to the y axis in a horizontal bar chart.
-             * The default horizontal bar configuration is specified in Chart.defaults.horizontalBar.
-             * http://www.chartjs.org/docs/latest/charts/bar.html
-             */
-            $yValue = str_replace('%', '', str_replace(',', '.', $objDb->{$fieldRendite}));
-            $chartY[] = '"' . $yValue . '"';
-            $chartX[] = '"' . $objDb->fondsname . '"';
+		$chartX = [];
+		$chartY = [];
+		$chartBg = [];
+		$chartBorder = [];
 
-            $color = $this->getColor($i);
-            $chartBg[] = "'rgba(" . implode(',', $color) . ",0.2)'";
-            $chartBorder[] = "'rgba(" . implode(',', $color) . ",1)'";
-            $i++;
-        }
+		$objDb = $this->Database->execute('SELECT * FROM tl_fonds_renditevergleich ORDER BY ' . $fieldRendite . ' DESC');
 
-        $this->Template->rows = $rows;
+		$i = 0;
+		$rows = array();
+		while ($objDb->next())
+		{
+			$strAnbieter = '';
+			$blnFound = false;
 
-        // Visible fields in the table
-        $fields = array('fondsname', 'isin');
-        $fields[] = $fieldRendite;
-        $fields = array_merge($fields, array('volantilitaet', 'laufendeKosten', 'factsheet', 'anbieter'));
+			// If there is a filter
+			if (\Input::get('anbieter') != '')
+			{
+				$strAnbieter = strtolower(trim($objDb->anbieter));
+				$arrAnbieter = explode(',', $strAnbieter);
+				$arrAnbieter = array_values($arrAnbieter);
+				$arrAnbieter = array_unique($arrAnbieter);
+				$arrAnbieter = array_filter($arrAnbieter);
+				if (in_array(strtolower(\Input::get('anbieter')), $arrAnbieter))
+				{
+					$rows[] = $objDb->row();
+					$blnFound = true;
+				}
+			}
+			else // Show all
+			{
+				$rows[] = $objDb->row();
+				$blnFound = true;
+			}
 
-        $this->Template->fields = $fields;
 
-        // Message, if there are no items
-        $this->Template->noItemsFound = $GLOBALS['TL_LANG']['MSC']['noItemFound'];
-        $this->Template->chartX = implode(',', $chartX);
-        $this->Template->chartY = implode(',', $chartY);
-        $this->Template->chartBg = implode(',', $chartBg);
-        $this->Template->chartBorder = implode(',', $chartBorder);
+			if ($blnFound)
+			{
+				/**
+				 * !important
+				 * The configuration options for the horizontal bar chart are the same as for the bar chart.
+				 * However, any options specified on the x axis in a bar chart, are applied to the y axis in a horizontal bar chart.
+				 * The default horizontal bar configuration is specified in Chart.defaults.horizontalBar.
+				 * http://www.chartjs.org/docs/latest/charts/bar.html
+				 */
+				$yValue = str_replace('%', '', str_replace(',', '.', $objDb->{$fieldRendite}));
+				$chartY[] = '"' . $yValue . '"';
+				$chartX[] = '"' . $objDb->fondsname . '"';
 
-        // Closure setValue from the template
-        $this->Template->setValue = (function ($key, $row)
-        {
-            $value = $row[$key];
-            switch ($key)
-            {
-                case 'factsheet':
-                    $value = ($value != '') ? '<a href="' . $value . '" target="_blank" title="zum Anbieter">Factsheet</a>' : '';
-                    break;
-                case 'anbieter':
-                    $value = sprintf('<img data-qtip2-tooltip="%s" style="cursor:help" src="system/modules/fiba_fonds_renditevergleich/assets/icons/list-provider.png">', $value);
-                    break;
-                case 'laufendeKosten':
-                case 'rendite3Jahre':
-                case 'rendite5Jahre':
-                case 'rendite10Jahre':
-                case 'volantilitaet':
-                    $value = str_replace('.', ',', $value);
-                    $value = str_replace('0,00', '', $value);
-                    $value = $value != '' ? $value . ' %' : '';
-                    break;
-            }
-            return $value;
-        });
+				$color = $this->getColor($i);
+				$chartBg[] = "'rgba(" . implode(',', $color) . ",0.2)'";
+				$chartBorder[] = "'rgba(" . implode(',', $color) . ",1)'";
+				$i++;
+			}
+		}
 
-        // Parse and output template for Ajax calls
-        if ($this->isAjax)
-        {
-            $this->Template->output();
-            exit();
-        }
+		$this->Template->rows = $rows;
 
-    }
+		// Visible fields in the table
+		$fields = array('fondsname', 'isin');
+		$fields[] = $fieldRendite;
+		$fields = array_merge($fields, array('volantilitaet', 'laufendeKosten', 'factsheet', 'anbieter'));
 
-    /**
-     * @param $num
-     * @return array
-     */
-    public function getColor($num)
-    {
-        $hash = md5('color' . $num); // modify 'color' to get a different palette
-        return array(
-            hexdec(substr($hash, 0, 2)), // r
-            hexdec(substr($hash, 2, 2)), // g
-            hexdec(substr($hash, 4, 2)) // b
-        );
-    }
+		$this->Template->fields = $fields;
 
-    /**
-     * @return array
-     */
-    protected function getAnbieter()
-    {
-        $strAnbieter = '';
-        $objDb = $this->Database->execute("SELECT * FROM tl_fonds_renditevergleich");
-        while ($objDb->next())
-        {
-            $strAnbieter .= trim($objDb->anbieter) . ',';
-        }
-        $arrAnbieter = explode(',', $strAnbieter);
+		// Message, if there are no items
+		$this->Template->noItemsFound = $GLOBALS['TL_LANG']['MSC']['noItemFound'];
+		$this->Template->chartX = implode(',', $chartX);
+		$this->Template->chartY = implode(',', $chartY);
+		$this->Template->chartBg = implode(',', $chartBg);
+		$this->Template->chartBorder = implode(',', $chartBorder);
 
-        // Remove empty
-        $arrAnbieter = array_values($arrAnbieter);
-        $arrAnbieter = array_unique($arrAnbieter);
-        $arrAnbieter = array_filter($arrAnbieter);
-        // Sort array
-        asort($arrAnbieter);
+		// Closure setValue from the template
+		$this->Template->setValue = (function ($key, $row)
+		{
+			$value = $row[$key];
+			switch ($key)
+			{
+				case 'factsheet':
+					$value = ($value != '') ? '<a href="' . $value . '" target="_blank" title="zum Anbieter">Factsheet</a>' : '';
+					break;
+				case 'anbieter':
+					$value = sprintf('<img data-qtip2-tooltip="%s" style="cursor:help" src="system/modules/fiba_fonds_renditevergleich/assets/icons/list-provider.png">', $value);
+					break;
+				case 'laufendeKosten':
+				case 'rendite3Jahre':
+				case 'rendite5Jahre':
+				case 'rendite10Jahre':
+				case 'volantilitaet':
+					$value = str_replace('.', ',', $value);
+					$value = str_replace('0,00', '', $value);
+					$value = $value != '' ? $value . ' %' : '';
+					break;
+			}
+			return $value;
+		});
 
-        return $arrAnbieter;
-    }
+		// Parse and output template for Ajax calls
+		if ($this->isAjax)
+		{
+			$this->Template->output();
+			exit();
+		}
+
+	}
+
+	/**
+	 * @param $num
+	 * @return array
+	 */
+	public function getColor($num)
+	{
+		$hash = md5('color' . $num); // modify 'color' to get a different palette
+		return array(
+			hexdec(substr($hash, 0, 2)), // r
+			hexdec(substr($hash, 2, 2)), // g
+			hexdec(substr($hash, 4, 2)) // b
+		);
+	}
+
+	/**
+	 * @return array
+	 */
+	protected function getAnbieter()
+	{
+		$strAnbieter = '';
+		$objDb = $this->Database->execute("SELECT * FROM tl_fonds_renditevergleich");
+		while ($objDb->next())
+		{
+			$strAnbieter .= trim($objDb->anbieter) . ',';
+		}
+		$arrAnbieter = explode(',', $strAnbieter);
+
+		// Remove empty
+		$arrAnbieter = array_values($arrAnbieter);
+		$arrAnbieter = array_unique($arrAnbieter);
+		$arrAnbieter = array_filter($arrAnbieter);
+		// Sort array
+		natcasesort($arrAnbieter);
+
+		return $arrAnbieter;
+	}
 
 
 }
+
+
 
 
 
